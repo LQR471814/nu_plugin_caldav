8344f784c0efd5210765a3dd5abd20a7f2890929
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-06-04 20:18:40 -0700

    feat(nix): flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    self.submodules = true;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.buildGoModule {
+        name = "nu_plugin_caldav";
+        system = builtins.currentSystem;
+        meta.mainProgram = "nu_plugin_caldav";
+
+        src = ./.;
+
+        vendorHash = "sha256-6AhxJPXHRpCRun4VaHGmT85ZBLR0fWHL0IKGVH+zaow=";
+        subPackages = [ "." ];
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/nu_plugin_caldav";
+      };
+    };
+}
