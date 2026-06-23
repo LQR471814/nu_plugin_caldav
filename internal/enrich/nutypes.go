c1b85c14adb05777fe806538821885a0b5150cda
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-06-23 07:36:50 -0700

    feat: add optional typing

--- internal/enrich/nutypes.go
+++ internal/enrich/nutypes.go
@@ -5,36 +5,40 @@ import (
 	"github.com/ainvaltin/nu-plugin/types"
 )
 
+func optionalType(t types.Type) types.Type {
+	return types.OneOf(t, types.Nothing())
+}
+
 var (
 	// EventRecordDef describes the Nushell record produced by Event.ToNu.
 	EventRecordDef = types.RecordDef{
 		"uid":                        types.String(),
-		"summary":                    types.String(),
-		"location":                   types.String(),
-		"description":                types.String(),
-		"categories":                 types.List(types.String()),
-		"datetime_stamp":             types.Record(nuconv.DatetimeType),
-		"created":                    types.Record(nuconv.DatetimeType),
-		"last_modified":              types.Record(nuconv.DatetimeType),
-		"class":                      nuconv.EventClassType,
-		"geo":                        types.Record(nuconv.EventGeoType),
-		"priority":                   types.Int(),
-		"sequence":                   types.Int(),
-		"status":                     nuconv.EventStatusType,
-		"transparency":               nuconv.EventTransparencyType,
-		"url":                        types.String(),
-		"comment":                    types.String(),
-		"attach":                     types.String(),
-		"contact":                    types.String(),
-		"organizer":                  types.String(),
+		"summary":                    optionalType(types.String()),
+		"location":                   optionalType(types.String()),
+		"description":                optionalType(types.String()),
+		"categories":                 optionalType(types.List(types.String())),
+		"datetime_stamp":             optionalType(types.Record(nuconv.DatetimeType)),
+		"created":                    optionalType(types.Record(nuconv.DatetimeType)),
+		"last_modified":              optionalType(types.Record(nuconv.DatetimeType)),
+		"class":                      optionalType(nuconv.EventClassType),
+		"geo":                        optionalType(types.Record(nuconv.EventGeoType)),
+		"priority":                   optionalType(types.Int()),
+		"sequence":                   optionalType(types.Int()),
+		"status":                     optionalType(nuconv.EventStatusType),
+		"transparency":               optionalType(nuconv.EventTransparencyType),
+		"url":                        optionalType(types.String()),
+		"comment":                    optionalType(types.String()),
+		"attach":                     optionalType(types.String()),
+		"contact":                    optionalType(types.String()),
+		"organizer":                  optionalType(types.String()),
 		"start":                      types.Record(nuconv.DatetimeType),
 		"end":                        types.Record(nuconv.DatetimeType),
-		"duration":                   types.Duration(),
-		"recurrence_rule":            nuconv.RRuleType,
-		"recurrence_dates":           types.Table(nuconv.DatetimeType),
-		"recurrence_exception_dates": types.Table(nuconv.DatetimeType),
-		"recurrence_instance":        types.Record(nuconv.DatetimeType),
-		"trigger":                    types.Record(nuconv.EventTriggerType),
+		"duration":                   optionalType(types.Duration()),
+		"recurrence_rule":            optionalType(nuconv.RRuleType),
+		"recurrence_dates":           optionalType(types.Table(nuconv.DatetimeType)),
+		"recurrence_exception_dates": optionalType(types.Table(nuconv.DatetimeType)),
+		"recurrence_instance":        optionalType(types.Record(nuconv.DatetimeType)),
+		"trigger":                    optionalType(types.Record(nuconv.EventTriggerType)),
 	}
 
 	// EventType is the Nushell type annotation for Event.
@@ -45,9 +49,9 @@ var (
 
 	// CalendarObjectRecordDef describes the Nushell record produced by CalendarObject.ToNu.
 	CalendarObjectRecordDef = types.RecordDef{
-		"object_path": types.String(),
+		"object_path": optionalType(types.String()),
 		"main":        EventType,
-		"overrides":   EventTableType,
+		"overrides":   optionalType(EventTableType),
 	}
 
 	// CalendarObjectType is the Nushell type annotation for CalendarObject.
